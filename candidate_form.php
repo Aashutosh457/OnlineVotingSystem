2e41e665f8e2600a127429c381d48591ef219d54
Aashutosh457 <aashu9003@gmail.com>
2023-07-14 22:07:37 +0545

    candidate form

--- candidate_form.php
+++ candidate_form.php
@@ -1,19 +1,23 @@
 <?php
-require("./connect.php");
+include 'connect.php';
 
 if (isset($_POST["submit"])) {
     $name = $_POST["name"];
     $position = $_POST["position"];
+    $image = $_FILES['image']['name'];
+    $image_tmp_name = $_FILES['image']['tmp_name'];
+    $image_folder = 'upload_img/'.$image;
     // $count = $_POST["count"];
     $search_query = "SELECT * FROM candidate WHERE name= '$name'";
     $res = $conn->query($search_query);
-    $sql="insert into  `candidate` (name,position) values('$name','$position')";
+    $sql="insert into  `candidate` (c_name,c_position,c_image) values('$name','$position','$image')";
     $result=mysqli_query($conn,$sql);
         if($result){
+            move_uploaded_file($image_tmp_name,$image_folder);
             echo "DATA HAS BEEN UPDATED SUCESSFULLY..";
             // header('location:candidate.php');
         }else{
-            die(mysqli_error($conn));
+            die(mysqli_error($conn));   
         }
       }
 
@@ -43,7 +47,7 @@ if (isset($_POST["submit"])) {
 
             <h2>Voting Site</h2>
         
-            <form action="#" method="post">
+            <form action="" method="post" enctype = "multipart/form-data">
                 <div class="form-field">
                     <label for="name">Candidated Name</label>
                     <input type="text" name="name" id="name" />
@@ -51,6 +55,10 @@ if (isset($_POST["submit"])) {
                 <div class="form-field">
                     <label for="position">Candidate Position</label>
                     <input type="name" name="position" id="position" />
+                </div>
+                <div class="formfield">
+                <label for="image">Candidate Photo</label>
+                    <input type="file" name="image" id="image" />
                 </div>
                 <div class="form-field">
                     <button type="submit" name="submit" class="btnLogin">Register</button>
